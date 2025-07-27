bf8c084984ada72ecb659f42a31be4b0e6ebd71f
mikias t <mteklu01@gmail.com>
2025-07-27 16:56:42 +0300

    update

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -118,7 +118,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.MessagePagination',  # Custom pagination class
+'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
 }
 
