ab111cd71cc70b7848a1027696520890eb01277f
mikias t <mteklu01@gmail.com>
2025-07-27 20:36:26 +0300

    Add RequestLoggingMiddleware to log user requests with timestamp, user, and path

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',  # DRF app
-    'chats',           # chats app
+    'apps.core.chats',          # chats app
     'django_filters',
     'rest_framework_simplejwt'  
 ]
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'apps.core.middleware.middleware.RequestLoggingMiddleware',
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
