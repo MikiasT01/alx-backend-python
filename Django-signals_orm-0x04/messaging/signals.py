50c703f83933d680ca185e390c81e61b42a640ad
mikias t <mteklu01@gmail.com>
2025-08-01 19:32:46 +0300

    Signal for deleting user-related data with delete_user view

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,7 @@
 # Django-signals_orm-0x04/messaging/signals.py
-from django.db.models.signals import pre_save, post_save
+from django.db.models.signals import pre_save, post_save, post_delete
 from django.dispatch import receiver
+from django.contrib.auth.models import User
 from .models import Message, Notification, MessageHistory
 
 @receiver(post_save, sender=Message)
@@ -10,16 +11,26 @@ def create_notification(sender, instance, created, **kwargs):
 
 @receiver(pre_save, sender=Message)
 def log_message_edit(sender, instance, **kwargs):
-    """Log the old content of a message before it’s updated."""
-    if instance.pk:  # Check if the instance already exists (i.e., being updated)
+    if instance.pk:
         try:
             old_message = Message.objects.get(pk=instance.pk)
             if old_message.content != instance.content and not old_message.edited:
                 MessageHistory.objects.create(
                     message=instance,
                     old_content=old_message.content,
-                    edited_by=instance.sender  # Assume sender is the editor
+                    edited_by=instance.sender
                 )
                 instance.edited = True
         except Message.DoesNotExist:
             pass
+
+@receiver(post_delete, sender=User)
+def cleanup_user_data(sender, instance, **kwargs):
+    """Clean up related data when a user is deleted."""
+    # Delete sent and received messages
+    instance.sent_messages.all().delete()
+    instance.received_messages.all().delete()
+    # Delete notifications for the user
+    Notification.objects.filter(user=instance).delete()
+    # Delete message histories where the user was the editor
+    MessageHistory.objects.filter(edited_by=instance).delete()
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,8 +1,14 @@
 # Django-signals_orm-0x04/messaging/views.py
-from django.shortcuts import render
-from .models import Message, MessageHistory
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import redirect
 
-def message_history(request, message_id):
-    message = Message.objects.get(id=message_id)
-    history = MessageHistory.objects.filter(message=message).order_by('edited_at')
-    return render(request, 'messaging/history.html', {'message': message, 'history': history})
+@login_required
+def delete_user(request):
+    """View to allow a user to delete their account."""
+    if request.method == 'POST':
+        user = request.user
+        user.delete()  # Triggers post_delete signal
+        return HttpResponse("Account deleted successfully.")
+    return redirect('home')  # Redirect to a home page (update URL config if needed)
